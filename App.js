b3c07eb5857af999ee952dd872a826bb04cd15ef
Rogerio <rogerio.silva2@alunos.sc.senac.br>
2023-02-28 13:47:01 -0300

    inicial

--- App.js
+++ App.js
@@ -1,21 +1,23 @@
-import * as React from 'react';
-import { View, Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { View } from "react-native";
+import { Button, Text } from "react-native-paper";
 
 export default function App() {
   return (
-    <HomeScreen />  
-  );
+    <RootNavigation />
+  )
+
 }
 
 const Stack = createNativeStackNavigator();
 
-function RootNavigation(){
-  return(
+function RootNavigation() {
+  return (
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="HomeScreen" component={HomeScreen} />
+        <Stack.Screen name="AboutScreen" component={AboutScreen}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
@@ -25,6 +27,15 @@ function HomeScreen() {
   return (
     <View>
       <Text> Ola seja bem vindo ao meu App</Text>
-    </View> 
+      <Button mode="contained">sobre</Button>
+    </View>
+  );
+}
+
+function AboutScreen(){
+  return (
+    <View>
+      <Text>Pagina Sobre</Text>
+    </View>
   )
 }
